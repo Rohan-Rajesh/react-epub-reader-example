89ace93d226619f88ac13b192b85aff2cd0fd32d
Rohan Rajesh <dev.rohan.rajesh@gmail.com>
2021-11-05 23:42:25 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const NodeRSA = require("node-rsa");
+const key = new NodeRSA({ b: 512 });
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/encrypt", (req, res) => {
+  const encrypted = key.encrypt(req.body.payload, "base64");
+  res.send(encrypted);
+});
+
+app.post("/decrypt", (req, res) => {
+  const decrypted = key.decrypt(req.body.payload, "utf8");
+  res.send(decrypted);
+});
+
+app.get("/*", (req, res) => res.render(path.join(__dirname, "./index.html")));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(9000, () => console.log(`Server started on port ${PORT}`));
